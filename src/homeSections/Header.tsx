ef75089e1055d275b33c9ee3ee7a033295580f2f
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-02-25 21:12:12 -0500

    changed pos of logo

--- src/homeSections/Header.tsx
+++ src/homeSections/Header.tsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
     <section className="">
-      <div className="relative flex justify-center items-center mt-5">
-        <h1 className="text-center font-bold text-2xl">Infinitic-tac-toe</h1>
+      <div className="relative flex pl-10 items-center mt-5">
+        <h1 className="text-center font-bold text-2xl">InfiniTic-Tac-Toe</h1>
 
         {/* <a className="absolute right-4 border border-indigo-500 p-1 px-3 font-semibold text-lg">
           Sign In
