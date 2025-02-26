ea41fb58811d898857e8d1b5f371f4ceafc1493e
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-02-25 21:14:27 -0500

    styled main menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { Sword, Swords } from "lucide-react";
 
 type MainMenuProps = {
   onModeSelect: (mode: "single" | "multi" | "online") => void;
@@ -8,23 +9,25 @@ type MainMenuProps = {
 export const MainMenu: React.FC<MainMenuProps> = ({ onModeSelect }) => {
   return (
     <div className="flex flex-col items-center justify-center h-[70vh]">
-      <h1 className="text-4xl font-bold mb-8">Tic Tac Toe</h1>
-      <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold mb-16">Choose Your Game Mode!</h1>
+      <div className="flex flex-col gap-6">
         <button
           onClick={() => onModeSelect("single")}
-          className="px-8 py-4 bg-gray-400 text-white rounded"
+          className="px-12 py-4 bg-violet-500 text-white btn-texts flex items-center justify-center"
         >
-          Single Player (Coming Soon)
+          Single Player
+          <Sword className="ml-2 mt-1" />
         </button>
         <button
           onClick={() => onModeSelect("multi")}
-          className="px-8 py-4 bg-indigo-500 text-white rounded"
+          className="px-12 py-4 bg-indigo-500 text-white btn-texts flex items-center justify-center"
         >
-          Multiplayer
+          <span>Multiplayer</span>
+          <Swords className="ml-2 mt-1" />
         </button>
         <button
           onClick={() => onModeSelect("online")}
-          className="px-8 py-4 bg-gray-400 text-white rounded"
+          className="px-12 py-4 bg-gray-400 text-white btn-texts"
         >
           Online (Coming Soon)
         </button>
