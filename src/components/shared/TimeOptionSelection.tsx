ab5bbea56b4b34e488f14c33b9401d4a89be37d4
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-03-03 19:39:06 -0500

    changed button styles

--- src/components/shared/TimeOptionSelection.tsx
+++ src/components/shared/TimeOptionSelection.tsx
@@ -16,16 +16,25 @@ export const TimeOptionSelection: React.FC<TimeOptionSelectionProps> = ({
       <div className="flex flex-col gap-4">
         <button
           onClick={() => onSelect(true)}
-          className="px-6 py-3 bg-blue-500 btn-texts hover:bg-blue-600"
+          className="px-8 py-4 bg-blue-500 btn-texts hover:bg-blue-600"
         >
           Timed
         </button>
         <button
           onClick={() => onSelect(false)}
-          className="px-6 py-3 bg-gray-500 btn-texts hover:bg-gray-600"
+          className="px-8 py-4 bg-gray-500 btn-texts hover:bg-gray-600"
         >
           No Time
         </button>
+      </div>
+      <div className="mt-6 font-semibold border bg-gray-100 p-4 rounded-lg">
+        <p className="text-center">What does choosing a time option mean?</p>
+        <br />{" "}
+        <p>
+          Whoever&apos;s turn it is they have a said amount of time to make
+          their move.
+          <br /> or else, it will be passed to the other player.
+        </p>
       </div>
     </div>
   );
